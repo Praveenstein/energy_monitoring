4d37cb79a27ab9791a7fd6bc68aaa2eb7025ab95
forcmti <smartmanufacturing@cmti-iiot.online>
2022-03-22 17:17:17 +0530

    added new functions to simulate current data and send data through socket programming to a tcp server, and also included  new module to simulated telegraf tcp server

--- energy_services/devices/simulator.py
+++ energy_services/devices/simulator.py
@@ -23,15 +23,20 @@ import plotly.graph_objects as go
 
 LOGGER = logging.getLogger(__name__)
 
-CHOICE_OF_STATUS = dict()
 
-# ((lower limit, upper limit of current value), (lower time, upper time),
-# (lower_average_current, upper_average_current),
-# (lower_peak_current, upper_peak_current), (key value mapping for machine status to code))
-# (0, 1, 2) ----> (machine off, machine on, machine production)
-CHOICE_OF_STATUS["MACHINE_OFF"] = ((0, 0.8), (180, 600), (0.25, 0.50), (0.65, 0.78), 0)
-CHOICE_OF_STATUS["MACHINE_ON"] = ((0.81, 3.5), (300, 900), (3.2, 3.5), (5.5, 7.5), 1)
-CHOICE_OF_STATUS["MACHINE_PRODUCTION"] = ((3.51, 10), (480, 1200), (3.51, 4.5), (10, 17), 2)
+def machine_states():
+
+    choice_of_states = dict()
+
+    # ((lower limit, upper limit of current value), (lower time, upper time),
+    # (lower_average_current, upper_average_current),
+    # (lower_peak_current, upper_peak_current), (key value mapping for machine status to code))
+    # (0, 1, 2) ----> (machine off, machine on, machine production)
+    choice_of_states["MACHINE_OFF"] = ((0, 0.8), (180, 600), (0.25, 0.50), (0.65, 0.78), 0)
+    choice_of_states["MACHINE_ON"] = ((0.81, 3.5), (300, 900), (3.2, 3.5), (5.5, 7.5), 1)
+    choice_of_states["MACHINE_PRODUCTION"] = ((3.51, 10), (480, 1200), (3.51, 4.5), (10, 17), 2)
+
+    return choice_of_states
 
 
 def generate_points(time_duration, status):
@@ -43,88 +48,92 @@ def generate_points(time_duration, status):
     return random_current_data
 
 
-def generate_current_data():
+def generate_current_data(choice_of_states):
 
-    # (machine status time change, state)
-    MACHINE_STATUS = [(0, 0), (181, 1)]
-    RAW_DATA = []
+    # (time of change of machine status, state)
+    overall_machine_status = [(0, 0), (181, 1)]
+    raw_data = []
 
-    initial_off_points = generate_points(180, CHOICE_OF_STATUS["MACHINE_OFF"])
-    initial_on_points = generate_points(300, CHOICE_OF_STATUS["MACHINE_ON"])
-    closing_off_points = generate_points(180, CHOICE_OF_STATUS["MACHINE_OFF"])
+    initial_off_points = generate_points(180, choice_of_states["MACHINE_OFF"])
+    initial_on_points = generate_points(300, choice_of_states["MACHINE_ON"])
+    closing_off_points = generate_points(180, choice_of_states["MACHINE_OFF"])
 
-    RAW_DATA.extend(initial_off_points)
-    RAW_DATA.extend(initial_on_points)
+    raw_data.extend(initial_off_points)
+    raw_data.extend(initial_on_points)
 
-    REMAINING_TIME = 28800
-    PREVIOUS_STATE = "MACHINE_ON"
-    PREVIOUS_TIME_FOR_STATE_CHANGE = 481
+    remaining_time = 28800
+    previous_state = "MACHINE_ON"
+    previous_time_for_state_change = 481
 
     while True:
-        status_key = random.choice(list(CHOICE_OF_STATUS.keys()))
-        if (PREVIOUS_STATE == "MACHINE_OFF") and (status_key == "MACHINE_PRODUCTION"):
+        status_key = random.choice(list(choice_of_states.keys()))
+        if (previous_state == "MACHINE_OFF") and (status_key == "MACHINE_PRODUCTION"):
             continue
-        if (PREVIOUS_STATE == "MACHINE_PRODUCTION") and (status_key == "MACHINE_OFF"):
+        if (previous_state == "MACHINE_PRODUCTION") and (status_key == "MACHINE_OFF"):
             continue
 
-        machine_status = CHOICE_OF_STATUS[status_key]
-        machining_time = random.randint(machine_status[1][0], machine_status[1][1])
+        current_machine_status = choice_of_states[status_key]
+        machining_time = random.randint(current_machine_status[1][0], current_machine_status[1][1])
 
-        print("============================")
-        print("Current Status: ", status_key)
-        print("Previous Status: ", PREVIOUS_STATE)
-        print("Time: ", machining_time)
-        REMAINING_TIME -= machining_time
+        LOGGER.info("============================")
+        LOGGER.info(f"Current Status: {status_key}")
+        LOGGER.info(f"Previous Status: {previous_state}")
+        LOGGER.info(f"Time: {machining_time}")
+        remaining_time -= machining_time
 
-        print("Remaining Time: ", REMAINING_TIME)
+        LOGGER.info(f"Remaining Time: {remaining_time}")
 
-        print("==============================")
+        LOGGER.info("==============================")
 
-        if REMAINING_TIME - 180 < 0:
-            REMAINING_TIME += machining_time
+        if remaining_time - 180 < 0:
+            remaining_time += machining_time
             break
 
-        if PREVIOUS_STATE == status_key:
-            PREVIOUS_TIME_FOR_STATE_CHANGE += machining_time
+        if previous_state == status_key:
+            previous_time_for_state_change += machining_time
         else:
-            MACHINE_STATUS.append((PREVIOUS_TIME_FOR_STATE_CHANGE, machine_status[-1]))
-            PREVIOUS_TIME_FOR_STATE_CHANGE += machining_time
+            overall_machine_status.append((previous_time_for_state_change, current_machine_status[-1]))
+            previous_time_for_state_change += machining_time
 
-        points = generate_points(machining_time, machine_status)
-        print("points: ", len(points))
-        RAW_DATA.extend(points)
-        PREVIOUS_STATE = status_key
+        points = generate_points(machining_time, current_machine_status)
+        raw_data.extend(points)
+        previous_state = status_key
 
-    closing_on_points = generate_points(REMAINING_TIME - 180, CHOICE_OF_STATUS["MACHINE_ON"])
-    RAW_DATA.extend(closing_on_points)
-    RAW_DATA.extend(closing_off_points)
+    closing_on_points = generate_points(remaining_time - 180, choice_of_states["MACHINE_ON"])
+    raw_data.extend(closing_on_points)
+    raw_data.extend(closing_off_points)
 
-    MACHINE_STATUS.append((MACHINE_STATUS[-1][0] + REMAINING_TIME, 1))
-    MACHINE_STATUS.append((MACHINE_STATUS[-1][0] + 180, 0))
+    overall_machine_status.append((overall_machine_status[-1][0] + remaining_time, 1))
+    overall_machine_status.append((overall_machine_status[-1][0] + 180, 0))
 
-    return RAW_DATA
+    return raw_data
 
 
 def main():
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as device_Socket:
+    tcp_server = "127.0.0.1"  # The server's hostname or IP address
+    tcp_server_port = 65432  # The port used by the server
+    try:
 
-        host = "localhost"
-        port = 1234
-        device_Socket.connect((host, port))
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_object:
+            socket_object.connect((tcp_server, tcp_server_port))
 
-        while True:
+            for _ in range(10):
+                choice_of_states = machine_states()
+                raw_data = generate_current_data(choice_of_states)
 
-            raw_data = generate_current_data()
+                for data in raw_data:
 
-            for data in raw_data:
-                message = {"device": "htm_stallion_200",
-                           "current": data,
-                           "time": time.time()}
-                message = json.dumps(message)
-                device_Socket.sendall(message.encode())
-                print("Sent")
-                time.sleep(1)
+                    message = {"device": "htm_stallion_200",
+                               "current": data,
+                               "time": time.time()}
+                    message = json.dumps(message)
+                    socket_object.sendall(message.encode())
+                    #data = socket_object.recv(1024)
+                    #print(f"Received {data.decode()}")
+                    time.sleep(2)
+    except Exception as error:
+        LOGGER.exception(error)
 
 
 if __name__ == "__main__":
---
+++ energy_services/devices/telegraf_simulator.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+DEVICE SIMULATOR
+======================
+
+Module for that contains functions to simulate a cnc machine
+
+This script requires that the following packages be installed within the Python
+environment you are running this script in.
+
+    * logging - to perform logging operations
+"""
+
+import logging
+import socket
+
+LOGGER = logging.getLogger(__name__)
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            print(f"Received {data.decode()}")
+            if not data:
+                break
+            #conn.sendall(data)
+
+
